46e819236e85c7a4583749e6c0f6cf12448151fd
Marina Baskova <marinabaskova@MacBook-Pro-11598.local>
2019-04-23 12:47:49 -0700

    added get by id route for posts

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -16,24 +16,25 @@ router.get('/', (req, res) => {
 		});
 });
 
+router.get('/:id', (req, res) => {
+	db
+		.findById(req.params.id)
+		.then((post) => {
+			console.log('GET by ID ', post);
+			if (post.length === 0) {
+				res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+			} else {
+				res.status(200).json(post);
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ error: 'The post information could not be retrieved.' });
+		});
+});
+
 module.exports = router;
 
 /* When the client makes a GET request to /api/posts:
 
 
-
-
- GET BY ID
-
-When the client makes a GET request to /api/posts/:id:
-
-If the post with the specified id is not found:
-
-return HTTP status code 404 (Not Found).
-return the following JSON object: { message: "The post with the specified ID does not exist." }.
-If there's an error in retrieving the post from the database:
-
-cancel the request.
-respond with HTTP status code 500.
-return the following JSON object: { error: "The post information could not be retrieved." }.
 */
